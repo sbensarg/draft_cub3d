9b1a91ab72a3ec7ac9b0c857448e81f2e1760728
Sara Bensarghin <sbensarg@e1r11p4.1337.ma>
2020-03-13 23:07:47 +0100

    draft cub3d

--- test.c
+++ test.c
@@ -14,33 +14,29 @@
 #define KEY_D 2
 #define PI 3.14159
 
-const		int tile_size = 32;
-const		int map_num_rows = 11;
-const		int map_num_cols = 15;
+const		int tile_size = 80;
+const		int map_num_rows = 7;
+const		int map_num_cols = 10;
 const		int window_width = map_num_cols * tile_size;
 const		int window_height = map_num_rows * tile_size;
 const		int ray_width = 1;
 const		int num_of_rays = window_width / ray_width;
 const		float fov_ang = 60 * (PI / 180);
-const		float	angleinc = (60 * (PI / 180)) / num_of_rays;
+const		float angleinc = (60 * (PI / 180)) / num_of_rays;
 const		float mini_map_scale_fact = 0.2;
 int			angleisup = 0;
 int			angleisleft = 0;
 int			angleisdown = 0;
 int			angleisright = 0;
 
-int grid[11][15] = {
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
-		{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
-		{1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1},
-		{1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+int grid[map_num_rows][map_num_cols] = {
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+		{1, 1, 1, 1, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 1, 1, 1, 0, 0, 0, 1}
 	};
 
 typedef struct
@@ -50,18 +46,31 @@ typedef struct
 }					data_t;
 typedef struct
 {
-	float					    x;
-	float 				    y;
-	float 			   radius;
-	float 		turndirection;
-	float		walkdirection;
-	float 		rotationangle;
-	float 			movespeed;
-	float 		rotationspeed;
-}					 player_t;
+	float	x;
+	float	y;
+	float	radius;
+	int		turndirection;
+	int		walkdirection;
+	float	rotationangle;
+	float	movespeed;
+	float	rotationspeed;
+}	player_t;
+
 
 player_t player;
 data_t        data;
+  
+void init_player(void)
+{
+	player.x = window_width / 2;
+	player.y = window_height / 2;
+ 	player.radius = 3;
+	player.turndirection = 0;
+	player.walkdirection = 0;
+	player.rotationangle = PI / 2;
+	player.movespeed = 8;
+	player.rotationspeed = 10 * (PI / 180);
+}
 void	vec(data_t data, int tileX, int tileY, int color)
 {
 	int	i;
@@ -87,18 +96,6 @@ void	vec(data_t data, int tileX, int tileY, int color)
 		j = -3;
 		i++;
 	}
-}
-
-void init_player(void)
-{
-	player.x = window_width / 2;
-	player.y = window_height / 2;
- 	player.radius = 3;
-	player.turndirection = 0;
-	player.walkdirection = 0;
-	player.rotationangle = PI / 2;
-	player.movespeed = 8;
-	player.rotationspeed = 10 * (PI / 180);
 }
 
 int		nextposallowed(float newplayerx, float newplayery);
@@ -242,19 +239,11 @@ float	putray(float angle)
 		i = hlenght;
 	else
 		i = vlenght;
-
-	while (i > 0)
-	{
-		xa = player.x - cos(angle) * (i);
-		ya = player.y - sin(angle) * (i);
-		mlx_pixel_put(data.mlx_ptr, data.mlx_win, mini_map_scale_fact * xa, mini_map_scale_fact * ya, 0x00FCC25F);	
-		i -= 1;
-	}
 	angleisup = 0;
 	angleisleft = 0;
 	angleisdown = 0;
 	angleisright = 0;
-	return (i);
+	return(i);
 }
 
 void	rect(data_t data, int tileX, int tileY, int color)
@@ -284,6 +273,41 @@ void	rect(data_t data, int tileX, int tileY, int color)
 	}
 }
 
+void	draw_wall(data_t data, int tileX, int tileY, int	height, int color)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (i < ray_width)
+	{
+		while (j * 2 <= tileY)
+		{
+			mlx_pixel_put(data.mlx_ptr, data.mlx_win, tileX + i, j, 0x006EBDEB);
+			mlx_pixel_put(data.mlx_ptr, data.mlx_win, tileX + i, tileY - j, 0x006EBDEB);
+			j++;
+		}
+		j = 0;
+		while (j <= height / 2)
+		{
+			mlx_pixel_put(data.mlx_ptr, data.mlx_win, tileX + i, tileY + j, color);
+			mlx_pixel_put(data.mlx_ptr, data.mlx_win, tileX + i, tileY + height - j, color);
+			j++;
+		}
+		j = 0;
+		while (tileY + height + (j * 2) <= window_height)
+		{
+			mlx_pixel_put(data.mlx_ptr, data.mlx_win, tileX + i, tileY + height + j, 0x00ABABAB);
+			mlx_pixel_put(data.mlx_ptr, data.mlx_win, tileX + i, window_height - j, 0x00ABABAB);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+}
+
+
 void	render_map(data_t data)
 {
 	int tileX;
@@ -292,7 +316,7 @@ void	render_map(data_t data)
 	int	i;
 	int	j;
 
-	i = 0;
+	i = 0;  
 	j = 0;
 	while(i < map_num_rows)
 	{
@@ -303,10 +327,10 @@ void	render_map(data_t data)
 			tileColor = grid[i][j] == 1 ? 0x00FF7F7F : 0x00FFFFFF;
 			if (grid[i][j] == 1)
 				rect(data, tileX, tileY, tileColor);
-			/*else
+			else
 			{
-				rect(data, tileX, tileY, 0x0);
-			}*/
+				rect(data, tileX, tileY, 0x00FFFFFF);
+			}
 			
 			j++;
 		}
@@ -320,15 +344,27 @@ void	render_rays()
 	float	first_ray_ang;
 	float	last_ray_ang;
 	float	ray_lenght;
-
-	first_ray_ang = player.rotationangle - (30 * (PI / 180));
-	last_ray_ang = player.rotationangle + (30 * (PI / 180));
+	float	correctdist;
+	float distanceprojectionplane;
+	float test;
+	int	wallstripheight;
+	int i;
+	i = 0;
+	first_ray_ang = player.rotationangle - (fov_ang / 2);
+	last_ray_ang = player.rotationangle + (fov_ang / 2);
 	while (first_ray_ang <= last_ray_ang)
 	{
 		ray_lenght = putray(first_ray_ang);
+		correctdist = ray_lenght * cos(first_ray_ang - player.rotationangle);
+		distanceprojectionplane = (window_width / 2) / tan(fov_ang / 2);
+		test = (tile_size / correctdist) * distanceprojectionplane;
+		wallstripheight = (int)test;
+		draw_wall(data, i * ray_width, (window_height / 2) - (wallstripheight / 2), wallstripheight, 0x00FFFFFF);
 		first_ray_ang += angleinc;
+		i++;
 	}
 }
+
 /*
 void render3dprojectedwalls()
 {
@@ -425,9 +461,10 @@ void	update()
 	if (player.turndirection == -1)
 		player.rotationangle -= player.rotationspeed;
 	mlx_clear_window(data.mlx_ptr, data.mlx_win);
-	render_map(data);
+	//render_map(data);
 	render_rays();
-	render_player(data, 5, 0x00FF7FFF);
+	//render_map(data);
+	//render_player(data, 5, 0x00FF7FFF);
 }
 
 int		ft_keyPressed(int keycode,void *param)
@@ -470,10 +507,10 @@ int main(void)
 		return (EXIT_FAILURE);
 	if ((data.mlx_win = mlx_new_window(data.mlx_ptr, window_width, window_height, "Hello world")) == NULL)
 		return (EXIT_FAILURE);
-	render_map(data);
-	render_player(data, 5, 0x00FF7FFF);
+	//render_map(data);
+	//render_player(data, 5, 0x00FF7FFF);
 	render_rays();
-	render3dprojectedwalls();
+	//render3dprojectedwalls();
 	mlx_hook(data.mlx_win, 2, 0, ft_keyPressed, (void *)0);
 	mlx_hook(data.mlx_win, 3, 0, ft_keyReleased, (void *)0);
 	mlx_loop(data.mlx_ptr);
